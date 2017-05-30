526930e75af04daa657a049c3838a85ccb3cadfa
Ty Fairclough <ty.fairclough@gmail.com>
2017-05-30 21:57:56 +0100

    restyled some of the shifts, some helpers and redid the month to show a week after and before

--- public/js/rision.js
+++ public/js/rision.js
@@ -1,5 +1,9 @@
 $(function()
 {
+    
+  $('[data-toggle="popover"]').popover()
+    
+    
 	$('.scroll-pane').jScrollPane();
     $('#ribbon a').click(function (e) {
   e.preventDefault()
@@ -25,34 +29,74 @@ $("#left-side-split").click(function(e){
         localStorage.setItem("shiftType",shiftType);
         $(".shifts li").removeClass("selected");
         $(this).addClass("selected")
+    $('#overlay').fadeOut(300, function(){
+        $('.expose').css('z-index','1');
+    });        
     })   
     
     $("main table td").click(function(e){
+        $('#shift').popover('destroy');
         $(this).removeClass();
-        $("div",this).removeClass();
-        if ( shiftType != null ) {
-        $("div",this).addClass("shift-"+shiftType);
+        $("div",this).removeClass();        
+        if ( shiftType === "clearShift") {
+            console.log("clearshift");            
+        } else if  ( shiftType != null ) {
+            $("div",this).addClass("shift-"+shiftType);
+        } else {
+            $('#shift').popover('show');
+                $(".expose").css('z-index','99999');
+                $('#overlay').fadeIn(300);
+            //alert("pick a shift type first");
         }
-    })
+    });
+    
+    
     
     
 $("table").click(function(e){
     e.preventDefault();
       activeTab('shift');
-
 });
     
     function activeTab(tab) {
               $('.nav-tabs a[href="#' + tab + '"]').tab('show');
     }
   
+    var fullmonth_array = ["Jan","Feb","March","April","May","June","July","August","September","October","November","December"];
+    
     
 $("#datepicker").datepicker( {
-    format: "mm-yyyy",
+    format: "mm MM",
     viewMode: "months", 
-    minViewMode: "months"
+    minViewMode: "months",
+    monthNamesShort: fullmonth_array
 });    
 
+
+$("td").hover(function(){
+    $("ul.staff li").removeClass();
+    var row = $(this).parent().index('tr') - 1;
+    $("ul.staff li").eq(row).addClass("selected");
+})  
+
+    
+
+    
+
+    /*
+    $("table td").on(
+        mouseenter: function() {
+            var row = $(this).parent().index('tr') - 1;
+            $("ul.staff li").eq(row).addClass("selected");
+            console.log('enter');
+        },
+        mouseleave: function() {
+            $("ul.staff li").removeClass();
+            console.log('bye!');
+        }    
+    );  
+    */
+    
 $.typeahead({
     input: '.js-typeahead-country_v1',
     order: "desc",
